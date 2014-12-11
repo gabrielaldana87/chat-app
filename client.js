8c44283ab5a5faf1939296a5b69ca08d9089d2dd
gabrielaldana87 <gabrielaldana87@gmail.com>
2014-12-11 16:51:10 -0500

    first commit of my client.js file

--- client.js
+++ client.js
@@ -72,7 +72,7 @@ ws.addEventListener("open",function(evt)
 ws.addEventListener("message",function(evt){
   addText(evt.data);
 });
-*/
+
 
 
 var ws = new WebSocket("ws://localhost:3000");
@@ -115,8 +115,7 @@ ws.addEventListener("message",function(evt){
 
 button.addEventListener("click",function(evt){
   ws.send(input.value);
-  addText("- "+input.value);
-  var addText()
+  //addText("- "+input.value);
   input.value="";
   var lis=document.querySelectorAll("li");
   for (i=0;i<lis.length;i++)
@@ -124,3 +123,55 @@ button.addEventListener("click",function(evt){
     lis[i].className="user";
     }
 })
+*/
+var ws = new WebSocket("ws://localhost:3000");
+var ul = document.createElement("ul");
+var body = document.querySelector("body");
+var input = document.createElement("input");
+var div = document.createElement("div");
+body.appendChild(input);
+body.appendChild(div);
+div.appendChild(input);
+var button = document.createElement("button");
+div.appendChild(button);
+button.innerText="Submit";
+body.appendChild(ul);
+
+var name = window.prompt("What is your name?").trim();
+
+var addText = function(msg)
+{
+  var newLi = document.createElement("li");
+  newLi.innerText=msg;
+  var FirstLi = ul.firstChild;
+  ul.insertBefore(newLi,FirstLi);
+}
+ws.addEventListener("open",function(evt)
+{
+  addText('connected');
+
+});
+
+ws.addEventListener("message",function(evt){
+  addText(evt.data);
+
+  var lit = document.querySelectorAll("li");
+  for (i=0;i<lit.length;i++)
+    {
+      lit[i].className="terminal";
+    }
+  });
+
+  button.addEventListener("click",function(evt){
+    var usercontent = {name:name};
+    usercontent["lines"] = input.value;
+    var messagecontent = JSON.stringify(usercontent);
+    ws.send(messagecontent);
+    //addText("- "+input.value);
+    input.value="";
+    var lis=document.querySelectorAll("li");
+    for (i=0;i<lis.length;i++)
+      {
+        lis[i].className="user";
+      }
+    })
